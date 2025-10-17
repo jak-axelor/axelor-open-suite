c7dab285f19290e8f0cab9ade595c21f021e028f
sos-axelor <8925217+sos-axelor@users.noreply.github.com>
2025-10-17 19:08:49 +0530

    RM#102486: Sale order line: Add 'Cost total' and 'Cost price'

--- axelor-sale/src/test/java/com/axelor/apps/sale/test/TestSaleOrderDiscountService.java
+++ axelor-sale/src/test/java/com/axelor/apps/sale/test/TestSaleOrderDiscountService.java
@@ -125,7 +125,8 @@ class TestSaleOrderDiscountService extends BaseTest {
         appSaleService,
         mock(ProductCompanyService.class),
         currencyScaleService,
-        mock(SaleOrderLineProductService.class));
+        mock(SaleOrderLineProductService.class),
+        currencyService);
   }
 
   protected SubSaleOrderLineComputeService createSubSaleOrderLineComputeService(
